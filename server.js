6e093e09d321fa20e91686ac5468d67217002f3e
Shivam Kumar <67912385+shivaom02@users.noreply.github.com>
2021-03-09 08:40:28 +0530

    User route and auth complete

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// connecting DB
+// Connecting DB
 connectDB();
 
+// Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send('API Running...'));
+
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
