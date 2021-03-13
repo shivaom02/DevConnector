f69da3fdd8d7a9d25a789141a3e0966e74c398ac
Shivam Kumar <67912385+shivaom02@users.noreply.github.com>
2021-03-13 18:18:30 +0530

    Register, Login and Logout functionality addeed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 // type 'impt' click enter
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [ formData, setFormData ] = useState({
         name: '',
         email: '',
@@ -22,8 +23,13 @@ const Register = ({ setAlert }) => {
         if(password !== password2) {
             setAlert('Passwords do not match!', 'danger');
         } else {
-            console.log('SUCESS!');
+            register({ name, email, password });
         }
+    }
+
+    //  Redirect if registered successfully
+    if (isAuthenticated) {
+        return <Redirect to='/dashboard' />
     }
 
     return (
@@ -35,13 +41,15 @@ const Register = ({ setAlert }) => {
                     <input type="text" placeholder="Name" name="name" 
                     value={name}
                     onChange={e => onChange(e)}
-                    required />
+                    // required 
+                    />
                 </div>
                 <div className="form-group">
                 <input type="email" placeholder="Email Address" name="email" 
                 value={email} 
                 onChange={e => onChange(e)} 
-                required />
+                // required 
+                />
                 <small className="form-text"
                     >This site uses Gravatar so if you want a profile image, use a
                     Gravatar email</small
@@ -52,7 +60,7 @@ const Register = ({ setAlert }) => {
                     type="password"
                     placeholder="Password"
                     name="password"
-                    minLength="6" 
+                    // minLength="6" 
                     value={password} 
                     onChange={e => onChange(e)} 
                 />
@@ -62,7 +70,7 @@ const Register = ({ setAlert }) => {
                     type="password"
                     placeholder="Confirm Password"
                     name="password2"
-                    minLength="6" 
+                    // minLength="6" 
                     value={password2} 
                     onChange={e => onChange(e)} 
                 />
@@ -77,7 +85,16 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
 };
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+    mapStateToProps, 
+    { setAlert, register }
+)(Register);
